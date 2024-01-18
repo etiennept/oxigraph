5f0d7de9bd487640335d92ddc99b66b9e0ea7ad8
etienne <etiennept@gmail.com>
2024-01-18 20:38:23 +0100

    minor echange

--- cli/templates/query.ts
+++ cli/templates/query.ts
@@ -2,7 +2,8 @@ import Yasgui from "@triply/yasgui";
 
 const url = window.location.href.endsWith('/') ? window.location.href.slice(0, -1) : window.location.href;
 
-new Yasgui(document.getElementById("yasgui")!!, {
+
+let yasgui =  new Yasgui(document.getElementById("yasgui")!!, {
     requestConfig: { endpoint: url + "/query" },
     endpointCatalogueOptions: {
         getData: function () {
@@ -12,5 +13,28 @@ new Yasgui(document.getElementById("yasgui")!!, {
             ];
         },
         keys: [],
-    }
+    } ,
+    //tabName : "config"
 });
+
+yasgui.on("tabAdd" , (instance: Yasgui, newTabId: string) =>{
+      //instance.config.tabName = "config"
+    //instance.tabElements.get(newTabId).tabEl!!.innerHTML ="<div> hello </div>"
+    instance.tabElements.addTab(newTabId ,   )
+    console.log("add tab")
+})
+yasgui.on("tabSelect" , (instance: Yasgui, newTabId: string) => {
+    instance.tabElements.get(newTabId)!!
+    console.log("select tab")
+})
+
+
+
+
+
+
+
+
+
+
+
